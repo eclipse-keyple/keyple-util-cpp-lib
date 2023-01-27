981fd37a8fa4393525fb8f7f5515e9c8a66467ee
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2023-01-27 09:33:53 +0100

    Fix: fixed keyple-cpp-examples 2022-05-30 on Linux / GCC

--- src/main/cpp/KeypleStd.h
+++ src/main/cpp/KeypleStd.h
@@ -53,9 +53,14 @@ inline std::ostream& operator<<(std::ostream& os, const std::vector<uint8_t>& v)
 
 inline std::ostream& operator<<(std::ostream& os, const std::vector<int>& v)
 {
-    for (int i = 0; i < static_cast<int>(v.size()); i++)
+    for (int i = 0; i < static_cast<int>(v.size()); i++) {
+        if (i != 0) {
+            os << ", ";
+        }
+
         os << std::hex << std::setfill('0') << std::setw(8)
            << static_cast<int>(v[i]);
+    }
 
     return os;
 }
