528ee27dff0378971367b51d1b1a69b5826af7e6
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2024-07-21 16:22:43 +0200

    feat: updated to 2.4.0

---
+++ include/keyple/core/util/cpp/exception/Exception.hpp
@@ -0,0 +1,132 @@
+/******************************************************************************
+ * Copyright (c) 2025 Calypso Networks Association https://calypsonet.org/    *
+ *                                                                            *
+ * This program and the accompanying materials are made available under the   *
+ * terms of the MIT License which is available at                             *
+ * https://opensource.org/licenses/MIT.                                       *
+ *                                                                            *
+ * SPDX-License-Identifier: MIT                                               *
+ ******************************************************************************/
+
+#pragma once
+
+#include <exception>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+namespace keyple {
+namespace core {
+namespace util {
+namespace cpp {
+namespace exception {
+
+class Exception : public std::exception {
+public:
+    /**
+     *
+     */
+    Exception()
+    {
+    }
+
+    /**
+     *
+     */
+    Exception(const Exception& o)
+    : mMessage(o.mMessage)
+    , mCause(o.mCause)
+    {
+    }
+
+    /**
+     *
+     */
+    explicit Exception(const std::string& message)
+    : mMessage(message)
+    , mCause(nullptr)
+    {
+    }
+
+    /**
+     *
+     */
+    Exception(
+        const std::string& message, const std::shared_ptr<Exception> cause)
+    : mMessage(message)
+    , mCause(cause)
+    {
+    }
+
+    /**
+     * Returns the detail message string of this exception.
+     */
+    const std::string&
+    getMessage() const
+    {
+        return mMessage;
+    }
+
+    /**
+     * Returns the cause of the exception.
+     */
+    const std::shared_ptr<Exception>
+    getCause() const
+    {
+        return mCause;
+    }
+
+    /**
+     *
+     */
+    const char*
+    what() const noexcept override
+    {
+        return mMessage.c_str();
+    }
+
+    /**
+     *
+     */
+    friend std::ostream&
+    operator<<(std::ostream& os, const Exception& e)
+    {
+        os << "EXCEPTION: {"
+           << "MESSAGE = " << e.mMessage;
+
+        if (e.mCause != nullptr) {
+            os << ", CAUSE = " << e.mCause->what();
+        }
+
+        os << "}";
+
+        return os;
+    }
+
+    /**
+     *
+     */
+    bool
+    operator==(const Exception& o) const
+    {
+        return mMessage == o.mMessage && mCause == o.mCause;
+    }
+
+private:
+    /**
+     *
+     */
+    const std::string mMessage;
+
+    /**
+     *
+     */
+    const std::shared_ptr<Exception> mCause;
+};
+
+} /* namespace exception */
+} /* namespace cpp */
+} /* namespace util */
+} /* namespace core */
+} /* namespace keyple */
