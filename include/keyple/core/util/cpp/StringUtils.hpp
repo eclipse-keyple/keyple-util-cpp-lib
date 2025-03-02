139fdb997840090e121483233db43055cebd359a
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2025-03-02 10:08:50 +0100

    wip

--- include/keyple/core/util/cpp/StringUtils.hpp
+++ include/keyple/core/util/cpp/StringUtils.hpp
@@ -1,20 +1,17 @@
 /**************************************************************************************************
- * Copyright (c) 2023 Calypso Networks Association https://calypsonet.org/ *
+ * Copyright (c) 2024 Calypso Networks Association https://calypsonet.org/                        *
  *                                                                                                *
- * See the NOTICE file(s) distributed with this work for additional information
- *regarding         * copyright ownership. *
+ * This program and the accompanying materials are made available under the                       *
+ * terms of the MIT License which is available at https://opensource.org/licenses/MIT.            *
  *                                                                                                *
- * This program and the accompanying materials are made available under the
- *terms of the Eclipse  * Public License 2.0 which is available at
- *http://www.eclipse.org/legal/epl-2.0                  *
- *                                                                                                *
- * SPDX-License-Identifier: EPL-2.0 *
+ * SPDX-License-Identifier: MIT                                                                   *
  **************************************************************************************************/
 
 #pragma once
 
 #include <algorithm>
 #include <cstdarg>
+#include <cstdio>
 #include <regex>
 #include <string>
 #include <vector>
