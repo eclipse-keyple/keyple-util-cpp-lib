07c8eefa6da43209a671ab96c0303720880e830b
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2025-04-03 11:23:29 +0200

    wip

--- include/keyple/core/util/cpp/exception/ArrayIndexOutOfBoundsException.hpp
+++ include/keyple/core/util/cpp/exception/ArrayIndexOutOfBoundsException.hpp
@@ -1,5 +1,5 @@
 /**************************************************************************************************
- * Copyright (c) 2024 Calypso Networks Association https://calypsonet.org/                        *
+ * Copyright (c) 2025 Calypso Networks Association https://calypsonet.org/                        *
  *                                                                                                *
  * This program and the accompanying materials are made available under the                       *
  * terms of the MIT License which is available at https://opensource.org/licenses/MIT.            *
